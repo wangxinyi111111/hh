1ed86b22b1e6317fa4f1911f79740203773bef2f
'mldn' <'mldnqa@163.com'>
2016-11-06 21:22:39 +0800

    合并

--- src/cn/mldn/ajax/servlet/EchoServlet.java
+++ src/cn/mldn/ajax/servlet/EchoServlet.java
@@ -18,7 +18,7 @@ public class EchoServlet extends HttpServlet {
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		out.println("Ettiiiiiiiiiiiii : " + request.getParameter("msg"));
+		out.println("Ettxxxxxxxxx : " + request.getParameter("msg"));
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
