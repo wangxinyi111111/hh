771c2596ac952a4a835c72229560c14aa7f56776
'mldn' <'mldnqa@163.com'>
2016-11-06 20:52:34 +0800

    增加了dev分支

---
+++ src/cn/mldn/ajax/servlet/eEchoServlet.java
@@ -0,0 +1,28 @@
+package cn.mldn.ajax.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = "/EchoServlet")
+public class eEchoServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("ECHO : " + request.getParameter("msg"));
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+}
